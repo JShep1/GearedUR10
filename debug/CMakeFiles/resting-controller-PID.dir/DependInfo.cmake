775650d8db8caf1c870df85a8ba77c91b74a8581
John <johnshepherd96@yahoo.com>
2016-06-08 15:20:16 -0400

    Initial commit with prototypedgears on arm in model.sdf file

---
+++ debug/CMakeFiles/resting-controller-PID.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/john/motioncontrolproject/motion-control-manipulator/resting-controller-PID.cpp" "/home/john/motioncontrolproject/motion-control-manipulator/debug/CMakeFiles/resting-controller-PID.dir/resting-controller-PID.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ debug/CMakeFiles/resting-controller.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/resting-controller.dir/resting-controller.cpp.o"
+  "libresting-controller.pdb"
+  "libresting-controller.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/resting-controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
