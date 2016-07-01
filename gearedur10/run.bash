d524aa4c6c790fb4d4bed15a0bbae471d434a597
John <johnshepherd96@yahoo.com>
2016-07-01 15:20:35 -0400

    Tried to tune gains for timestep 01 and 1

--- gearedur10/run.bash
+++ gearedur10/run.bash
@@ -4,7 +4,7 @@ cd debug/
 make
 cd ..
 cd gearedur10/
-moby-driver -s=0.001 -mt=3 -p=/home/john/GearedUR10/debug/libsinusoidal-controller.so ur10-geared.xml
+moby-driver -s=0.01  -mt=3 -p=/home/john/GearedUR10/debug/libsinusoidal-controller.so ur10-geared.xml
 python plot_PD_PID.py base_gear_joint
 python plot_PD_PID.py arm_gear_joint
 python plot_PD_PID.py upperarm_gear_joint
